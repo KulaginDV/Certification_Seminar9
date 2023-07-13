dfe776d2f5b19debace6f6d4904a4295ab2a6af9
kulaginDV <kulagin1104@yandex.ru>
2023-07-13 13:17:13 +0300

    decision task64

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 64. Задать значение N. Написать программу, 
+которая выведет все натуральные числа в промежутке 
+от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void NaturalToLow(int n, int score)
+{
+    if (n < 0)
+        Console.WriteLine("Your number is negative!");
+    else
+    {
+    if (score > n)
+        {
+            return;
+        }
+    else
+        {
+            NaturalToLow(n, score + 1);
+            Console.Write(score + " ");
+        }
+    }
+}
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
