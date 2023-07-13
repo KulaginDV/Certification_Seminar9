99da314603e3177f85ede34e3ddf9517f7ecddf9
kulaginDV <kulagin1104@yandex.ru>
2023-07-13 13:58:30 +0300

    decision task66

--- task66/Program.cs
+++ task66/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 66. 
+Задать значения M и N. Написать программу, которая 
+найдёт сумму натуральных элементов в промежутке 
+от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int FindSum (int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + FindSum(m, n);
+        return res;
+    }
+}
+
+void PrintSum (int m, int n)
+{
+    Console.Write(FindSum(m-1, n));
+}
+
+Console.Write("Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintSum(m, n);
